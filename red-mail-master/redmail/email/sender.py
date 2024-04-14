758fc266c8710d82f4eaf855a740f3c89c11e2da
Udhav-Saraswat <udhav.saraswat.17@gmail.com>
2024-04-14 15:32:48 +0530

    DEV-01 : change

--- red-mail-master/redmail/email/sender.py
+++ red-mail-master/redmail/email/sender.py
@@ -149,7 +149,6 @@ class EmailSender:
     templates_text_table.globals["is_last_group_row"] = is_last_group_row
 
     attachment_encoding = 'UTF-8'
-
     def __init__(self,
                  host:str,
                  port:int,
